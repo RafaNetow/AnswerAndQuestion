b6087ce37ffade3d83a9f6f9d4fb52d1a0467d0d
LAPTOP-O1P5B9D9\Sequeirios <rafanetow@gmail.com>
2017-06-15 12:12:43 -0600

    responder preguntas y  contestarlas funciona correctamente

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @questions = Question.order(created_at: :desc).all;
   end
 
   def about
 
   end
-end
+
+  def temp
+     redirect_to root_path
+  end
+
+  def question
+
+
+  end
+  end
+
---
+++ app/models/question.rb
@@ -0,0 +1,7 @@
+class Question < ApplicationRecord
+    has_many:answers
+
+  def gravatar
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  end
+end
